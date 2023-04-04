55d7be7bf4a1ae5e4da9eec34b6b9a365c2b1c9b
somu146 <somnath.pansare@neosoftmail.com>
2023-04-04 19:04:29 +0530

    root to index screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
   resources :teams, only:[:index, :show]
 
   mount Sidekiq::Web => '/sidekiq'
